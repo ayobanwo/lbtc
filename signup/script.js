81c1c66a6dded59a47526771aab53cabc3ac2332
ayobanwo <olubanwoayomide@gmail.com>
2022-07-24 17:44:53 +0100

    new commit

--- signup/script.js
+++ signup/script.js
@@ -1,18 +1,25 @@
 let submitButton  = document.querySelector('.signIn-btn');
 
 submitButton.addEventListener('click', e => {
+    let firstName = document.querySelector('#firstName').value;
+    let lastName = document.querySelector('#lastName').value;
     let email = document.querySelector('#email').value;
     let password = document.querySelector('#password').value;
-
+    let password2 = document.querySelector('#password2').value;
+    let phone = document.querySelector('#phone').value;
 
     let dataObj = {
+        firstName,
+        lastName,
         email,
         password,
+        password2,
+        phone
     }
 
     console.log(dataObj);
 
-    fetch("https://lightstream-app.herokuapp.com/api/user/login", {
+    fetch("https://lightstream-app.herokuapp.com/api/user/register", {
         method: 'post',
         body: JSON.stringify(person),
         headers: {
